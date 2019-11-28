110b972c9b30698f30e26f75c65db906afd41811
Macbook Tobias <tobias_landman@hotmail.com>
2019-11-28 10:29:34 +0100

    h

--- api/model/functions.js
+++ api/model/functions.js
@@ -9,7 +9,7 @@ const fecha = require('fecha');
 const Cryptr = require('cryptr');
 const cryptr = new Cryptr(keys.encryptionKey);
 //const crypto=require('crypto');
-const bcrypt = require('bcrypt');
+//const bcrypt = require('bcrypt');
 const saltRounds = 10; // for bcrypt encryption
 // Some functions to help validate things or to convert values to right formats etc
 // useable for all model files
